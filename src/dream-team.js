52ffe4c17755b2f2c680fdd0791b77070a9ef6b5
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-12 01:34:52 +0300

    feat: implementation of function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if(members == undefined) {
+		return '';
+	}
+	else {
+		var answer = "";
+		for(var i = 0; i < members.length; i++) {
+			var member = members[i];
+			if(typeof(member) == "string") {
+				var ans = member.split('');
+				for(var j = 0; j < ans.length; j++) {
+					if(ans[j] != " ") {
+						answer += ans[j];
+						break;
+					}
+				}
+			}
+		}
+		answer = answer.toUpperCase();
+		return answer.split('').sort().join('');
+	}
 };
