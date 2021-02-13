92e636271d445bf3addc6a8268a7395d0e1df6d7
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-13 23:50:20 +0300

    feat: implementation of function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	var answer = "";
+	if(options.separator == undefined) {
+		options.separator = "+";
+	}
+	if(options.additionSeparator == undefined) {
+		options.additionSeparator = "|";
+	}
+	if(options.addition === undefined) {
+		options.addition = "";
+	}
+	else {
+		options.addition = String(options.addition);
+	}
+	if(options.repeatTimes == null || options.repeatTimes == undefined) {
+		options.repeatTimes = 1;
+	}
+	if(options.additionRepeatTimes == null || options.additionRepeatTimes == undefined) {
+		options.additionRepeatTimes = 1;
+	}
+	for(var j = 0; j < options.repeatTimes; j++) {
+		answer += "" + String(str);
+		for(var i = 0; i < options.additionRepeatTimes; i++) {
+			answer += "" + String(options.addition);
+			if(i != options.additionRepeatTimes - 1) {
+				answer += "" + options.additionSeparator;
+			}
+		}
+		if(j != options.repeatTimes - 1) {
+			answer += "" + options.separator;
+		}
+	}
+	return answer;
 };
   
