c7d42a4f0366a3dce36a5b4fde068674ecaf3f5c
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-17 23:23:22 +0300

    feat: add function setChain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,22 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [], 
+  reversed: false,
   getLength() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
+  
+  setChain() {
+    this.chain = []
+    this.reversed = false
+  },
+  
   addLink(value) {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
+  
   removeLink(position) {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
+  
   reverseChain() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
+  
   finishChain() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
