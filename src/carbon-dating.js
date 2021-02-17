c3ead1416ff1bb2c6cc2f3361de26baf1969f3bd
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-17 19:34:15 +0300

    feat: edit function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,10 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-	if(typeof parseInt(sampleActivity) != "int" || sampleActivity == undefined) {
+	if(sampleActivity == undefined || sampleActivity == null) {
+		return false;
+	}
+	if(!(sampleActivity instanceof String)) {
 		return false;
 	}
 };
