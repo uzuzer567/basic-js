325562fb461c7b1e5cfb98173ab389e2c8e35c1c
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-21 00:34:05 +0300

    feat: implementation of function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!(arr instanceof Array)) {
+	throw new RangeError();
+  }
+  
+  var transformArray = [];
+ 
+  for (var i = 0; i < arr.length; i++) {
+		if (arr[i] !== '--discard-prev' && arr[i] !== '--discard-next' && arr[i] !== '--double-prev' && arr[i] !== '--double-next') {
+			transformArray.push(arr[i]);
+		}
+		else {
+			if(arr[i] === '--discard-next') {	 
+				if(i != arr.length - 1){
+					  if(arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev') {
+						  i += 2;
+					  }
+					  else {
+						  i ++;
+					  }
+				}
+			}
+			else if(arr[i] === '--discard-prev') {
+				  if(i != 0) {
+					  transformArray.pop();
+				  }
+			}
+			else if(arr[i] === '--double-prev') {
+				  if(i != 0) {
+					  transformArray.push(arr[i - 1]);
+				  }
+			}
+			else if(arr[i] === '--double-next') {
+				  if(i != arr.length - 1) {
+					  transformArray.push(arr[i + 1]);
+				  }
+			}
+		}
+  }
+  return transformArray;
 };
