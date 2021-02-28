e798508131348db3966a88bef9dd0d83bdb47f1a
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-28 16:25:03 +0300

    fix: edit function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,10 +4,11 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-	if(sampleActivity == undefined || sampleActivity == null) {
+	if(sampleActivity == undefined || sampleActivity < 0 || typeof sampleActivity !== "string" || MODERN_ACTIVITY < sampleActivity) {
 		return false;
 	}
-	if(!(sampleActivity instanceof String)) {
+	if(!parseFloat(sampleActivity)) {
 		return false;
 	}
+	return Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / Math.log(2) * HALF_LIFE_PERIOD);
 };
