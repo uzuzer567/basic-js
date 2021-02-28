4bb06e752db9d57a2e01c90517fbaef2a5ad7d11
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-28 15:10:05 +0300

    feat: implementation of function encrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,8 +5,54 @@ class VigenereCipheringMachine {
 	this.crypt = crypt === false ? false : true;
   }
   encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  	  if (message == undefined || key == undefined) {
+		  throw new Error();
+	  }
+	  
+	  this.getTable();
+	  	 
+	  let answer = [];
+	  let messageArray = message.toString().toUpperCase().split('');
+	  let keyArray = key.toString().toUpperCase();
+	  
+	  while(message.length > keyArray.length) {
+		keyArray += key.toUpperCase();
+	  }
+	  
+	  keyArray = keyArray.split('');
+
+	  let column = [];
+	  let row = [];
+	  
+	  for (let i = 0; i < messageArray.length; i++) {
+		  column.push(alphabet.indexOf(messageArray[i]));
+	  }
+	  
+	  for (let i = 0; i < messageArray.length; i++) {
+		  row.push(alphabet.indexOf(keyArray[i]));
+	  }
+	  
+	  let notLetterCounter = 0;
+	  
+	  for(let i = 0; i < messageArray.length; i++) {
+		  if(messageArray[i] >= "A" && messageArray[i] <= "Z" && notLetterCounter === 0) {
+			answer.push(table[column[i]][row[i]]);
+		  }
+		  else if(messageArray[i] >= "A" && messageArray[i] <= "Z" && notLetterCounter !== 0) {
+			answer.push(table[column[i]][row[i - notLetterCounter]]);
+		  }
+		  else {
+			answer.push(messageArray[i]);
+			notLetterCounter++;
+		  }
+	  }
+	  	  
+	  if(this.crypt) {
+		  return answer.join('');
+	  }
+	  else {
+	      return answer.reverse().join('');
+	  }
   }    
   decrypt() {
     throw new CustomError('Not implemented');
