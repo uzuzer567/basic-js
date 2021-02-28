db92d638b1b62125ba4c89b474b2023ea378dfbb
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-28 15:21:43 +0300

    feat: add alphabet and table

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
+let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let table = [];
+
 class VigenereCipheringMachine {
   constructor (crypt) {
 	this.crypt = crypt === false ? false : true;
