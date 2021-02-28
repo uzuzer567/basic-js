53fd51f49d9512513ddfc932da4b55e80a536a8e
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-02-28 15:07:35 +0300

    feat: add function getTable

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -9,6 +9,21 @@ class VigenereCipheringMachine {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
+  getTable() {
+	  for (let i = 0; i < alphabet.length; i++) {
+		table[i] = new Array(alphabet.length);
+	  }
+	  for (let i = 0; i < alphabet.length; i++) {
+		for (let j = 0; j < alphabet.length; j++) {
+			let shift = i + j;
+			if(shift >= alphabet.length) {
+				shift = shift % alphabet.length;
+			}
+			table[i][j] = alphabet[shift];
+		}
+	  }
+	  return table;
+  }
 }
 
 module.exports = VigenereCipheringMachine;
